9d0c02868001455edc47448b56bf561b691a1c2a
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-02-21 11:40:14 +0300

    refactor: delete pagination

--- client/src/pages/blog/index.tsx
+++ client/src/pages/blog/index.tsx
@@ -1,44 +1,19 @@
 import axios from 'axios';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import Articles from '../../components/Articles';
 import Background from '../../components/UI/Background';
-import Pagination from '../../components/UI/Pagination';
 import Section from '../../components/UI/Section';
 import Title from '../../components/UI/Title';
-import { getPagesArray } from '../../utils/pages';
 import ArticlesSkeleton from '../../components/UI/ArticlesSkeleton';
 
 export default function BlogPage() {
-  const router = useRouter();
   const [isLoading, setLoading] = useState(false);
-  const startLoading = () => setLoading(true);
-  const stopLoading = () => setLoading(false);
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     axios.get('/api/articles').then(({ data }) => setPosts(data));
   }, []);
-  // const changePage = (page) => {
-  //   const currentPath = router.pathname;
-  //   const currentQuery = router.query;
-  //   currentQuery.page = page;
-  //   router.push({
-  //     pathname: currentPath,
-  //     query: currentQuery,
-  //   });
-  // };
-
-  // useEffect(() => {
-  //   router.events.on('routeChangeStart', startLoading);
-  //   router.events.on('routeChangeComplete', stopLoading);
-  //   return () => {
-  //     router.events.off('routeChangeStart', startLoading);
-  //     router.events.off('routeChangeComplete', stopLoading);
-  //   };
-  // });
-  // const pagesArray = getPagesArray(pageCount);
 
   return (
     <>
@@ -54,7 +29,6 @@ export default function BlogPage() {
         <Title variant={'align-center'}>Блог</Title>
         <div className="blog__content">
           {isLoading ? <ArticlesSkeleton num={6} /> : <Articles posts={posts} />}
-          {/* <Pagination pagesArray={pagesArray} page={currentPage} changePage={changePage} /> */}
         </div>
       </Section>
     </>
