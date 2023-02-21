78edb9355e450478023f9a4097966bbc20d59c42
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-02-21 17:52:33 +0300

    feat: add telegram bot

--- src/pages/api/application.ts
+++ src/pages/api/application.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 import nodemailer from 'nodemailer';
 
@@ -7,7 +8,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const email = req.body.email;
     const day = req.body.day;
     const discipline = req.body.discipline;
-
+    axios
+      .post(
+        'https://api.telegram.org/bot5828813441:AAFlrLmqyD-SgIOZ4D_m0ZUWMN_SqCf-abg/sendMessage',
+        {
+          chat_id: -837433131,
+          text: `Оставлена новая заявка
+Имя: ${name}
+Email: ${email}
+День недели: ${day}
+Тренировка: ${discipline}`,
+        }
+      )
+      .catch((err) => console.log(err));
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
@@ -15,7 +28,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         pass: 'ggyzwjgrfdewrpim',
       },
     });
-
     const mailOptions = {
       from: 'hfgym.minsk@gmail.com',
       to: 'olya.sidoruk98@gmail.com',
@@ -26,7 +38,6 @@ Email: ${email}
 День недели: ${day}
 Тренировка: ${discipline}`,
     };
-
     transporter.sendMail(mailOptions).then(() => {
       res.status(200).json({ success: true });
     });
