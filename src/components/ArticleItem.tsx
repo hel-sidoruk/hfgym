976eda120ee6160b2cbc6efd976a10a9153692c1
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-03-06 22:02:20 +0300

    feat: add loader for post

--- src/components/ArticleItem.tsx
+++ src/components/ArticleItem.tsx
@@ -3,22 +3,27 @@ import Text from './UI/Text';
 import Title from './UI/Title';
 import ImageAnimated from './UI/ImageAnimated';
 import { PostInterface } from '@/types';
+import { Loader } from './UI/Loader';
 
 const ArticleItem = ({ post }: { post: PostInterface | null }) => {
   return (
     <Section sectionName="article-item__block">
       <div className="article-item__content">
-        <Title variant="article-item__title">{post ? post.title : ''}</Title>
-        <div>
-          {post && (
-            <ImageAnimated
-              src={`/images/articles/${post.image}`}
-              alt={post.title}
-              className="article-item__image"
-            />
-          )}
-          <Text variant="article-item__text">{post ? post.text : ''}</Text>
-        </div>
+        {post ? (
+          <>
+            <Title variant="article-item__title">{post.title}</Title>
+            <div>
+              <ImageAnimated
+                src={`/images/articles/${post.image}`}
+                alt={post.title}
+                className="article-item__image"
+              />
+              <Text variant="article-item__text">{post.text}</Text>
+            </div>
+          </>
+        ) : (
+          <Loader />
+        )}
       </div>
     </Section>
   );
