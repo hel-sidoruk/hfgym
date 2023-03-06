90e6379d2b914d841912150fab70ae414ba843a6
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-03-06 18:33:51 +0300

    fix: fix preloader

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,28 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import Footer from './Footer';
 import { Header } from './Header';
+import { Preloader } from './UI/Preloader';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const [showPreloader, setShowPreloader] = useState(true);
+  const { pathname } = useRouter();
+  useEffect(() => {
+    setTimeout(() => setShowPreloader(false), 7000);
+  }, []);
+
+  useEffect(() => {
+    pathname !== '/' && setShowPreloader(false);
+  }, [pathname]);
+
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      {showPreloader && <Preloader />}
       <main>{children}</main>
       <Footer />
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,16 +7,11 @@ import SignUp from '../components/SignUp';
 import News from '../components/News';
 import axios from 'axios';
 import { ApplicationForm } from '@/components/ApplicationForm';
-import { Preloader } from '@/components/UI/Preloader';
+// import { Preloader } from '@/components/UI/Preloader';
 // import { Popup } from '@/components/UI/Popup';
 
 export default function Home() {
   const [news, setNews] = useState([]);
-  const [showPreloader, setShowPreloader] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => setShowPreloader(false), 7000);
-  }, []);
 
   useEffect(() => {
     axios.get('/api/articles').then(({ data }) => setNews(data));
@@ -31,7 +26,6 @@ export default function Home() {
           content="Тренировки по Бразильскому джиу-джитсу, Грэпплингу, Вольной борьбе, Тайскому боксу, ММА. Единоборства в Минске для детей и взрослых. Бразильское джиу-джитсу и грэпплинг Минск. Тайский бокс Минск. ММА Минск. Вольная борьба Минск"
         />
       </Head>
-      {showPreloader && <Preloader />}
       <Hero home={true} />
       <AboutHome />
       <Classes />
