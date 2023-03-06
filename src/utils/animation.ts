a39f24001e7c569e4bdbb5f9712b69244401cf86
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-03-06 18:02:23 +0300

    feat: add preloader for home page

---
+++ src/utils/animation.ts
@@ -0,0 +1,25 @@
+export function animateSVG() {
+  let len = 0;
+  let delay = 0.5;
+  const speed = 9;
+
+  const paths = document.querySelectorAll('.path_line') as NodeListOf<SVGGeometryElement>;
+
+  paths.forEach((element) => {
+    const elementLen = element.getTotalLength();
+    console.log(elementLen);
+    len += elementLen;
+  });
+
+  paths.forEach((element) => {
+    const elementLen = element.getTotalLength();
+    const duration = (elementLen / len) * speed;
+
+    element.style.animationDuration = `${duration}s`;
+    element.style.animationDelay = `${delay}s`;
+
+    element.setAttribute('stroke-dasharray', `${elementLen}`);
+    element.setAttribute('stroke-dashoffset', `${elementLen}`);
+    delay += duration - 0.7;
+  });
+}
