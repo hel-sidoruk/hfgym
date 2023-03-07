9d9c1a9a2a6649bd3ecf87d1f93f0037829cd95c
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-03-07 15:41:13 +0300

    feat: add iframe video

--- src/components/AboutContent.tsx
+++ src/components/AboutContent.tsx
@@ -1,5 +1,6 @@
 import { aboutTexts } from '../utils/aboutPageText';
 import AboutTextBlock from './AboutTextBlock';
+import Text from './UI/Text';
 import Title from './UI/Title';
 
 const AboutContent = () => {
@@ -9,6 +10,17 @@ const AboutContent = () => {
       {aboutTexts.map(({ textArray, position }) => (
         <AboutTextBlock key={position} textArray={textArray} position={position} />
       ))}
+      <div className="about__film">
+        <Text>Короткометражный фильм о Holy Family Gym.</Text>
+        <iframe
+          className="frame"
+          src="https://www.youtube.com/embed/maH5q8Avlew"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
     </section>
   );
 };
